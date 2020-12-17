8438c619ad82dac3446acb9ca69436261169f336
dotsquid <dotsquid@gmail.com>
2020-12-17 23:33:55 +0200

    Add start tip

---
+++ Assets/Scripts/Input/TipController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TipController : MonoBehaviour
+{
+    private static readonly int kRunTriggerHash = Animator.StringToHash("Run");
+
+    [SerializeField]
+    private Animator _animator;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.5f);
+        while (Input.touchCount == 0 && !Input.GetMouseButtonDown(0))
+            yield return null;
+        _animator.SetTrigger(kRunTriggerHash);
+    }
+}
